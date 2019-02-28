2a8a25ccc5a8793d812d71b1cbbbb8556a9f1763
pulasthi <pulasthi911@gmail.com>
2019-02-28 14:54:07 -0500

    adding initial code

---
+++ src/main/java/BloombergStats.java
@@ -0,0 +1,38 @@
+import edu.iu.dsc.tws.api.task.ComputeConnection;
+import edu.iu.dsc.tws.api.task.TaskGraphBuilder;
+import edu.iu.dsc.tws.api.task.TaskWorker;
+import edu.iu.dsc.tws.comms.api.Op;
+import edu.iu.dsc.tws.data.api.DataType;
+import edu.iu.dsc.tws.executor.api.ExecutionPlan;
+import edu.iu.dsc.tws.task.api.BaseSink;
+import edu.iu.dsc.tws.task.api.BaseSource;
+import edu.iu.dsc.tws.task.api.IMessage;
+import edu.iu.dsc.tws.task.graph.DataFlowTaskGraph;
+
+public class BloombergStats extends TaskWorker {
+
+    public void execute() {
+        int parallism = 192;
+        String filePath = "/share/project2/FG546/pulasthi/bloomberg";
+        String outFilePath = "/share/project2/FG546/pulasthi/bloomberg/toutput";
+
+        BaseSource readSource = new DataReadSourceTask("edge",filePath);
+        BaseSink resultSink = new SinkTask();
+        TaskGraphBuilder taskGraphBuilder = TaskGraphBuilder.newBuilder(config);
+        taskGraphBuilder.addSource("source", readSource, parallism);
+        ComputeConnection computeConnection = taskGraphBuilder.addSink("sink", resultSink, 1);
+        computeConnection.reduce("source", "edge", Op.SUM, DataType.DOUBLE);
+
+        DataFlowTaskGraph dataFlowTaskGraph = taskGraphBuilder.build();
+        ExecutionPlan executionPlan = taskExecutor.plan(dataFlowTaskGraph);
+        taskExecutor.execute(dataFlowTaskGraph, executionPlan);
+    }
+
+    protected class SinkTask extends BaseSink {
+        private static final long serialVersionUID = -254264903510284798L;
+
+        public boolean execute(IMessage message) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/DataReadSourceTask.java
@@ -0,0 +1,50 @@
+import edu.iu.dsc.tws.task.api.BaseSource;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DataReadSourceTask extends BaseSource {
+    private String edge;
+    private String inputDir;
+    private String filePrefirx = "part_";
+    private double sum;
+    private double count;
+    public DataReadSourceTask(String edge, String inputDir) {
+        this.edge = edge;
+        this.inputDir = inputDir;
+    }
+
+    public void execute() {
+        try {
+            int currentMax = -1;
+            int fileIndex = context.taskIndex();
+            String fileId = (fileIndex < 100) ? "0" : "";
+            fileId += (fileIndex < 10) ? "0" + fileIndex : "" + fileIndex;
+            String filePath = inputDir + filePrefirx + fileId;
+            BufferedReader bf = new BufferedReader(new FileReader(filePath));
+            String line = null;
+            String splits[];
+
+            while (count < 40000000 && (line = bf.readLine()) != null) {
+                splits = line.split("\\s+");
+                int row = Integer.valueOf(splits[0]);
+                int col = Integer.valueOf(splits[1]);
+                double value = Double.valueOf(splits[2]);
+                sum += value;
+
+                count++;
+                if (context.getWorkerId() == 0 && (count % 20000000 == 0)) {
+                    System.out.print(".");
+                }
+                if (currentMax > row) {
+                    throw new IllegalStateException("File : " + fileId + " not in order at line : " + count);
+                }
+                currentMax = row;
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
