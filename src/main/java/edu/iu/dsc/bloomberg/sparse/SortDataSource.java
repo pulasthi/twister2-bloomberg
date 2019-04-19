2856b5897fd52506864931f0b6eec7b32d24097b
pulasthi <pulasthi911@gmail.com>
2019-04-19 12:10:31 -0400

    updating code

--- src/main/java/edu/iu/dsc/bloomberg/sparse/SortDataSource.java
+++ src/main/java/edu/iu/dsc/bloomberg/sparse/SortDataSource.java
@@ -36,7 +36,7 @@ public class SortDataSource extends BaseSource {
     public void prepare(Config cfg, TaskContext ctx) {
         super.prepare(cfg, ctx);
         int fileIndex = ctx.taskIndex();
-        filePrefix = "/scratch_hdd/bloomberg/part_" + fileIndex + "__";
+        filePrefix = "/scratch_hdd/bloomberg/part_" + fileIndex + "_merg_sorted";
         data = new HashMap<>();
 //        dupCounts = new HashMap<>();
     }
@@ -44,86 +44,56 @@ public class SortDataSource extends BaseSource {
     @Override
     public void execute() {
         String line = null;
+        int prow = 0, pcol = 0, pval = 0;
+        boolean readFirst = false;
+
+
         int row;
         int col;
         int val;
-        for (int i = 0; i < rounds; i++) {
-            String file = filePrefix + i;
-            try {
-                bf = new BufferedReader(new FileReader(file));
-
-                while ((line = bf.readLine()) != null) {
-                    splits = line.split("\\s+");
-                    row = Integer.valueOf(splits[0]);
-                    col = Integer.valueOf(splits[1]);
-                    val = Integer.valueOf(splits[2]);
-
-                    if (data.containsKey(row)) {
-                        TreeMap<Integer, Integer> temp = data.get(row);
-                        if (temp.containsKey(col)) {
-                            temp.put(col, temp.get(col) / 2 + val / 2);
-                            countdoubles++;
-//                            if (dupCounts.containsKey(row)) {
-//                                if (dupCounts.get(row).containsKey(col)) {
-//                                    dupCounts.get(row).put(col, dupCounts.get(row).get(col) + 1);
-//                                } else {
-//                                    dupCounts.get(row).put(col, 2);
-//                                }
-//                            } else {
-//                                dupCounts.put(row, new HashMap<>());
-//                                dupCounts.get(row).put(col, 2);
-//                            }
-                        } else {
-                            temp.put(col, val);
-                        }
-                    } else {
-                        TreeMap<Integer, Integer> temp = new TreeMap<>();
-                        temp.put(col, val);
-                        data.put(row, temp);
-                    }
-                }
-                bf.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-//        for (Integer integer : dupCounts.keySet()) {
-//            for (Map.Entry<Integer, Integer> s : dupCounts.get(integer).entrySet()) {
-//                if(s.getValue() > 2){
-//                    LOG.info("####### " + integer + " " + s.getKey() + " " + s.getValue());
-//                }
-//            }
-//        }
-        //Now sort and print
-        LOG.info("Done Loading data");
-        int[] sorted = new int[data.keySet().size()];
-        Object[] keys = data.keySet().toArray();
-        for (int i = 0; i < sorted.length; i++) {
-            sorted[i] = (Integer) keys[i];
-        }
-
-        Arrays.sort(sorted);
-        LOG.info("Total number of rows " + sorted.length);
         try {
-            PrintWriter outWriter = new PrintWriter(new FileWriter(filePath + "sorted_part_" + context.taskIndex()));
+            PrintWriter outWriter = new PrintWriter(new FileWriter(filePrefix + "_summed"));
 
-            for (Integer sortedrow : sorted) {
-                TreeMap<Integer, Integer> temp = data.get(sortedrow);
-                for (Map.Entry<Integer, Integer> entry : temp.entrySet()) {
-                    outWriter.println(sortedrow + " " + entry.getKey() + " " + entry.getValue());
+            String file = filePrefix;
+            bf = new BufferedReader(new FileReader(file));
+            if (!readFirst) {
+                line = bf.readLine();
+                splits = line.split("\\s+");
+                prow = Integer.valueOf(splits[0]);
+                pcol = Integer.valueOf(splits[1]);
+                pval = Integer.valueOf(splits[2]);
+                readFirst = true;
+            }
+
+            while ((line = bf.readLine()) != null) {
+                splits = line.split("\\s+");
+                row = Integer.valueOf(splits[0]);
+                col = Integer.valueOf(splits[1]);
+                val = Integer.valueOf(splits[2]);
+
+                if (prow == row && pcol == col) {
+                    outWriter.println(row + " " + col + " " + (pval / 2 + val / 2));
+                    prow = row;
+                    pcol = col;
+                    pval = val;
+                } else {
+                    outWriter.println(prow + " " + pcol + " " + pval);
+                    prow = row;
+                    pcol = col;
+                    pval = val;
                 }
             }
+
+            bf.close();
             outWriter.flush();
             outWriter.close();
 
+            //LOG.info("Total number of rows " + sorted.length);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-        LOG.info("Count doubles " + countdoubles);
         context.write(this.EDGE, new double[]{countdoubles});
         context.end(this.EDGE);
     }
